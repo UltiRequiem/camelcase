8a9e96103b732c415bdc634fbfc2fba00f47ade3
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-07 21:48:56 -0500

    :sparkles: Node.js Support

--- mod.ts
+++ mod.ts
@@ -6,6 +6,45 @@ import {
 } from "./mod_utils.ts";
 import type { CamelCaseOptions } from "./mod_types.ts";
 
+/**
+Convert a dash/dot/underscore/space separated string to camelCase or PascalCase: `foo-bar` → `fooBar`.
+Correctly handles Unicode strings.
+@param input - String to convert to camel case.
+@example
+```js
+import camelCase = require('camelcase');
+camelCase('foo-bar');
+//=> 'fooBar'
+camelCase('foo_bar');
+//=> 'fooBar'
+camelCase('Foo-Bar');
+//=> 'fooBar'
+camelCase('розовый_пушистый_единорог');
+//=> 'розовыйПушистыйЕдинорог'
+camelCase('Foo-Bar', {pascalCase: true});
+//=> 'FooBar'
+camelCase('--foo.bar', {pascalCase: false});
+//=> 'fooBar'
+camelCase('Foo-BAR', {preserveConsecutiveUppercase: true});
+//=> 'fooBAR'
+camelCase('fooBAR', {pascalCase: true, preserveConsecutiveUppercase: true}));
+//=> 'FooBAR'
+camelCase('foo bar');
+//=> 'fooBar'
+console.log(process.argv[3]);
+//=> '--foo-bar'
+camelCase(process.argv[3]);
+//=> 'fooBar'
+camelCase(['foo', 'bar']);
+//=> 'fooBar'
+camelCase(['__foo__', '--bar'], {pascalCase: true});
+//=> 'FooBar'
+camelCase(['foo', 'BAR'], {pascalCase: true, preserveConsecutiveUppercase: true})
+//=> 'FooBAR'
+camelCase('lorem-ipsum', {locale: 'en-US'});
+//=> 'loremIpsum'
+```
+*/
 export function camelCase(
   input: string | string[],
   options?: CamelCaseOptions,
