4a245fed453b38d5943ffa1b8b4caffd1aa37282
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-22 22:06:49 -0500

    :bookmark: 2.1.0

---
+++ scripts/node.ts
@@ -0,0 +1,10 @@
+import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.15/node.ts";
+
+buildPackage({
+  repoName: "camelcase",
+  description:
+    "Convert a dash/dot/underscore/space separated string to camelCase or PascalCase: `foo-bar` → `fooBar`",
+  homepage: "https://camelcase.js.org",
+  keywords: ["camelcase", "camel-case", "text"],
+  version: "2.1.0",
+});
