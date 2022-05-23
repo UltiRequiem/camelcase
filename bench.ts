66001769fddcf3f5b80998482993e787630e086b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-22 22:01:39 -0500

    :memo: Benchmarks

---
+++ bench.ts
@@ -0,0 +1,22 @@
+import sindreCamelCase from "https://esm.sh/camelcase@6.3.0";
+
+import { camelCase as thisModuleCamelCase } from "./mod.ts";
+
+const loremIpsum = `Lorem Ipsum is simply dummy text of the 
+printing and typesetting industry. Lorem Ipsum has been the
+industry's standard dummy text ever since the 1500s, when
+an unknown printer took a galley of type and scrambled it to
+make a type specimen book. It has survived not only five centuries,
+but also the leap into electronic typesetting, remaining
+essentially unchanged. It was popularised in the 1960s with the release
+of Letraset sheets containing Lorem Ipsum passages, and more recently with
+desktop publishing software like Aldus PageMaker
+including versions of Lorem Ipsum.`;
+
+Deno.bench("Sindre Sorhus Module", { group: "camelcase" }, () => {
+  sindreCamelCase(loremIpsum);
+});
+
+Deno.bench("This Module", { group: "camelcase" }, () => {
+  thisModuleCamelCase(loremIpsum);
+});
